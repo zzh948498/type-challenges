65fbb785f022aac8cea5a9a44a7308af93e99836
“张智皓” <zhang948498@163.com>
2021-12-17 17:58:13 +0800

    feat: 🎸 ReplaceKeys

--- questions/1130-medium-replacekeys/template.ts
+++ questions/1130-medium-replacekeys/template.ts
@@ -1,1 +1,9 @@
-type ReplaceKeys<U, T, Y> = any
+type ReplaceKeys<U, T, Y> = U extends U
+  ? {
+    [Key in keyof U]: Key extends T
+      ? Key extends keyof Y
+        ? Y[Key]
+        : never
+      : U[Key];
+  }
+  : never
